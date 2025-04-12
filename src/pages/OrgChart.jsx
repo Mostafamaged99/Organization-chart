2781091314ea50c246d818c3711b170b133a9524
mostafamaged99 <mostafaabdelmaged98@gmail.com>
2025-04-12 22:52:41 +0200

    FEAT: Add wiremock endpoint for organization demo data

--- src/pages/OrgChart.jsx
+++ src/pages/OrgChart.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { OrganizationChart } from "primereact/organizationchart";
 import { Modal, Button, Form } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function ColoredDemo() {
   const [data, setData] = useState([
